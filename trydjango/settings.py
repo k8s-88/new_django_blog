1fcea654e2131e52298e6729c2178059070d8a13
Katie O Donnell <katiemodonnell88@gmail.com>
2019-02-22 12:42:19 +0000

    users app created

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['new-django-blog-katiemodonnell88.c9users.io']
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
